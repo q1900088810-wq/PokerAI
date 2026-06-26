605fc83742439474e5b0c3522a191952dfae716f
q1900088810-wq <q1900088810@gmail.com>
2026-06-27 01:35:51 +0800

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,1 +1,14 @@
-document.body.innerHTML += "<h2 style='color:red'>JS加载成功</h2>";
+// ===== 52张牌 =====
+
+const ranks = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"];
+const suits = ["♠","♥","♦","♣"];
+
+let deck = [];
+
+for (let r of ranks){
+    for (let s of suits){
+        deck.push(r+s);
+    }
+}
+
+console.log(deck);
